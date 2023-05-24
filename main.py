872c41597392e2fae39eb2c450607ed61b6d46e0
MercuryGryph <w84627302@gmail.com>
2023-05-24 22:09:01 +0800

    -

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import pygame
 import sys
 
-color_bg = [255, 255, 255]
+# 定义几个颜色备用
+COLOR_bg = [255, 255, 255]
+COLOR_red = [255, 85, 85]
 
+# 初始化
 pygame.init()
-pygame.display.set_caption("[2048]")
-
+# 窗口标题
+pygame.display.set_caption("[GAME]")
+# 窗口大小
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
-screen.fill(color_bg)
-
+# 背景颜色
+screen.fill(COLOR_bg)
+# 基本时钟
 clock = pygame.time.Clock()
 clock.tick(20)
 
+# 主循环
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         pygame.display.update()
+
