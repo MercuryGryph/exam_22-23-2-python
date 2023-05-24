f34bbd0a67e3367cef40a878726397f87701a3b4
MercuryGryph <w84627302@gmail.com>
2023-05-25 03:21:07 +0800

    -

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import pygame.freetype
-import sys
+import random
 
 # 初始化
 pygame.init()
@@ -43,33 +43,51 @@ column = 20
 #  格尺寸
 tile_size = 25
 #  偏移
-drift_T = 20
+drift_T = tile_size
 drift_L = 0
 
-frame_L = (width - row * tile_size) / 2 + drift_L
+frame_L = (width - column * tile_size) / 2 + drift_L
 frame_R = width - frame_L + drift_L * 2
-frame_T = (height - column * tile_size) / 2 + drift_T
+frame_T = (height - row * tile_size) / 2 + drift_T
 frame_B = height - frame_T + drift_T * 2
 #  内框
 #   列
-for i in range(1, 20):
+for i in range(1, column):
     pygame.draw.line(screen, color['line'], (frame_L + tile_size * i, frame_T), (frame_L + tile_size * i, frame_B), 3)
 #   行
-for i in range(1, 20):
+for i in range(1, row):
     pygame.draw.line(screen, color['line'], (frame_L, frame_T + tile_size * i), (frame_R, frame_T + tile_size * i), 3)
 #  外框
 pygame.draw.line(screen, color['frame'], (frame_L, frame_T), (frame_L, frame_B), 3)
 pygame.draw.line(screen, color['frame'], (frame_L, frame_T), (frame_R, frame_T), 3)
 pygame.draw.line(screen, color['frame'], (frame_R, frame_T), (frame_R, frame_B), 3)
 pygame.draw.line(screen, color['frame'], (frame_L, frame_B), (frame_R, frame_B), 3)
-
+#  逻辑网格
+plant = []
+for i in range(0, row):
+    plant.append([])
+    for j in range(0, column):
+        plant[i].append(0)
+# 蛇
+snake_head = [row//2, column//2]
+plant[snake_head[0]][snake_head[1]] = -1
+snake_tail = []
 # 文字
 default_font.render_to(screen, (frame_L, frame_T - tile_size), "Score:", color['gray'], color['none'])
+
+
+def get_tile_pos_dest(pos):
+    x = pos[0]
+    y = pos[1]
+    px = frame_L + x * tile_size
+    py = frame_T + y * tile_size
+    return px, py
 
 
 def main():
     # 主循环
     is_gaming = True
+    is_fruit_exist = False
     score = 1234567890
     while is_gaming:
         # 操作处理
@@ -87,6 +105,10 @@ def main():
                 pass
 
         # 逻辑处理
+        #  生成水果
+        if not is_fruit_exist:
+            pass
+        #  更新画面
         default_font.render_to(screen, (frame_L + tile_size * 5, frame_T - tile_size),
                                "{0}".format(score), color['gray'], color['none'])
         pygame.display.update()
